d74ff43e3442841264fc2caaf8a6bec0780d3a11
TheGameMstr <elijiahmarsano44@gmail.com>
2026-08-29 22:08:38 +0000

    frequency map

---
+++ FrequencyMap.py
@@ -0,0 +1,24 @@
+# Character Frequency Map: Take a string input and return a dictionary
+# showing exactly how many times each character appears.
+user_input = "hello world and this is a storry about a cute girl named kambria"
+def freq_map_space(user_input):
+    freq_dict = {}
+    for let in user_input:
+        if let not in freq_dict.keys():
+            freq_dict[let] = 1
+        else:
+            freq_dict[let] += 1
+    return freq_dict
+
+def freq_map(user_input):
+    user_input = user_input.replace(" ","")
+    freq_dict = {}
+    for let in user_input:
+        if let not in freq_dict.keys():
+            freq_dict[let] = 1
+        else:
+            freq_dict[let] += 1
+    return freq_dict
+
+print(freq_map_space(user_input))
+print(freq_map(user_input))
